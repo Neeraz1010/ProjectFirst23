f64e09295dd387b5748954c35f00bba3f5737fc9
Niraj <bca210634_niraj@achsnepal.edu.np>
2023-05-13 20:07:03 +0545

    Added userLogin in Database

--- project23.sql
+++ project23.sql
@@ -1,1 +1,10 @@
-CREATE DATABASE medixo;
+CREATE DATABASE medixoHospital;
+USE medixoHospital;
+
+CREATE TABLE userLogin (
+		id INT NOT NULL AUTO_INCREMENT,
+        userId VARCHAR(255) NOT NULL,
+		fullName VARCHAR(255) NOT NULL,
+		phoneNumber VARCHAR(255) NOT NULL,
+        PRIMARY KEY(id)
+);
